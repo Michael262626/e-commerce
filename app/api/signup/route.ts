c22f4bc3ca91642d5e0fc3e47079712c26ed9aa5
Michael262626 <dikandumichael@gmail.com>
2025-05-29 10:41:54 +0100

    Commit

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../../../lib/generated/prisma/client'
 import bcrypt from "bcrypt"
 
 const prisma = new PrismaClient()
